58fec3725071f32082ce1ae0371daa6c41efac65
Devin1824 <devster18@gmail.com>
2023-01-11 19:28:16 -0800

    Week 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +9,131 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            
+            foreach(int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    count += number;
+                }
+                else
+                {
+                    count -= number;
+                }
+            }
+            return count;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<string> list = new List<string> { str1, str2, str3, str4 };
+            string shortestString = list.OrderBy(x => x.Length).First();
+            return shortestString.Length;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> numbers = new List<int> { number1, number2, number3, number4 };
+            return numbers.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if(sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double result;
+            bool confirmed = double.TryParse(input, out result);
+            if(confirmed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int nullCount = 0;
+            int objCount = 0;
+            for(int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i] == null)
+                {
+                    nullCount++;
+                }
+                else
+                {
+                    objCount++;
+                }
+            }
+            if(nullCount > objCount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            double numTotal = 0;
+            double numCount = 0;
+            if(numbers == null)
+            {
+                return 0;
+            }
+            foreach(double num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    numTotal += num;
+                    numCount++;
+
+                }
+            }
+            double avg = numTotal / numCount;
+            if(numCount == 0)
+            {
+                avg = 0;
+            }
+            return avg;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("number", "number can not be negative");
+            }
+            int result = 1;
+            while(number != 0)
+            {
+                result *= number;
+                number--;
+            }
+            
+            return result;
         }
     }
 }
